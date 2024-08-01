03a037abcbb199d784e497122421892db4fdf91e
claudiooleite <claudio.leite@hotmail.com>
2024-08-01 13:11:50 +0200

    styling cards

--- src/app/components/ingredientsForm.js
+++ src/app/components/ingredientsForm.js
@@ -12,29 +12,30 @@ function IngredientForm({ onAddIngredient }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="bg-orange-300 w-full h-44 p-4 flex flex-col items-center">
-        <h1 className="text-lg font-semibold">
-          What do you have in your pantry?
-        </h1>
-        <div className="relative mt-4 flex items-center">
-          <span
-            type="submit"
-            className="material-icons-outlined absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-            onClick={handleSubmit}>
-            add
-          </span>
-          <input
-            type="text"
-            value={ingredientName}
-            onChange={(e) => setIngredientName(e.target.value)}
-            className="pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Add an ingredient"
-          />
-        </div>
+    <form
+      onSubmit={handleSubmit}
+      className="bg-orange-300 w-full h-44 p-4 flex flex-col items-center border-b-2 border-b-neutral-950 rounded-b-xl">
+      <h1 className="text-lg font-semibold">
+        What do you have in your pantry?
+      </h1>
+      <div className="relative mt-4 flex items-center">
+        <span
+          type="submit"
+          className="material-icons-outlined absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
+          onClick={handleSubmit}>
+          add
+        </span>
+        <input
+          type="text"
+          value={ingredientName}
+          onChange={(e) => setIngredientName(e.target.value)}
+          className="pl-10 pr-4 py-2 border-2 border-neutral-950 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Add an ingredient"
+        />
       </div>
     </form>
   );
 }
 
 export default IngredientForm;
+
--- src/app/components/recipes.js
+++ src/app/components/recipes.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
+import Image from "next/image";
 
 // Website credentials
 const KEY = "974f7f2b0bd545bbbd319a94fac1a359";
@@ -9,14 +10,17 @@ const URL = "https://api.edamam.com/api/recipes/v2";
 function Recipes({ pantry }) {
   const [recipes, setRecipes] = useState([]);
 
-  const pantryItems = pantry.map((item) => item.name).join(" ");
+  const pantryItems = useMemo(
+    () => pantry.map((item) => item.name).join(" "),
+    [pantry],
+  );
 
   // Fetch recipes when pantry items change
   useEffect(() => {
-    if (pantryItems) {
+    if (pantryItems || !pantry.length) {
       getRecipes();
     }
-  }, [pantryItems]);
+  }, [pantryItems, pantry.length]);
 
   function getRecipes() {
     const query = encodeURIComponent(pantryItems);
@@ -33,7 +37,30 @@ function Recipes({ pantry }) {
       <h1>Recipes</h1>
       <ul>
         {recipes.slice(0, 4).map((recipe, index) => (
-          <li key={index}>Title: {recipe.recipe.label}</li>
+          <>
+            <li key={index}>
+              <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+                <Image
+                  src={recipe.recipe.image}
+                  alt="card-image"
+                  width={150}
+                  height={300}
+                />
+                <div class="p-6">
+                  <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                    {recipe.recipe.label}
+                  </h5>
+                </div>
+                <div class="p-6 pt-0">
+                  <button
+                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                    type="button">
+                    Read More
+                  </button>
+                </div>
+              </div>
+            </li>
+          </>
         ))}
       </ul>
     </>
