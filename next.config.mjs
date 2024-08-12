59b7250690e085acdc745a943b12cb4668f7a3df
claudiooleite <claudio.leite@hotmail.com>
2024-08-12 09:49:54 +0200

    port added

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'edamam-product-images.s3.amazonaws.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "edamam-product-images.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
-      // Add more patterns if needed for other domains
     ],
   },
 };
