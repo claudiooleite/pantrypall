0869831b25578edcf8223f92ba4de307ceca0140
claudiooleite <claudio.leite@hotmail.com>
2024-08-12 09:40:05 +0200

    image.domains configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["edamam-product-images.s3.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'edamam-product-images.s3.amazonaws.com',
+        pathname: '/**',
+      },
+      // Add more patterns if needed for other domains
+    ],
   },
 };
 
--- src/components/recipeCard/RecipeCard.js
+++ src/components/recipeCard/RecipeCard.js
@@ -1,9 +1,10 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 const RecipeCard = ({ id, image, label, mealType, calories }) => {
   return (
     <Link href={`/recipe/${id}`}>
+      {console.log(image)}
       <div className="m-2 p-2 rounded-lg w-72 md:w-80 lg:w-96 flex flex-col items-center cursor-pointer">
         <div className="relative w-full h-40 md:h-48 lg:h-56 border-2 border-neutral-950 rounded-lg overflow-hidden">
           <Image
@@ -11,7 +12,7 @@ const RecipeCard = ({ id, image, label, mealType, calories }) => {
             alt={label}
             fill
             priority
-            style={{ objectFit: 'cover' }}
+            style={{ objectFit: "cover" }}
             className="rounded-lg"
             sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
